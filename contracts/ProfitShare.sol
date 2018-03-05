97ea773ab37393872e48200489ff6d6cfa84a435
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2018-03-05 10:02:01 +0330

    initial commit

---
+++ contracts/ProfitShare.sol
@@ -0,0 +1,180 @@
+pragma solidity ^0.4.15;
+
+import 'zeppelin-solidity/contracts/token/ERC20/ERC20Basic.sol';
+import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+
+contract ProfitShare is StandardToken{
+    using SafeMath for uint256;
+
+    string public name = "ProfitShare Token";
+    string public symbol = "PShare";
+    uint256 public decimals = 18;
+
+    // 267M
+    uint256 public totalSupply = 267*1000000 * (uint256(10) ** decimals);
+    uint256 public totalRaised; // total ether raised (in wei)
+
+    uint256 public totalShareable; // total shareable ethers (in wei)
+    uint256 public totalActiveCoins; // total active coins (in wei)
+
+    // 1 ETH = 87245 PShare
+    uint256 public coinPerETH = 87245;
+
+    /**
+     * List of all users
+     */
+    address[] public allUsers;
+
+    /**
+     * Address which will receive raised funds 
+     * and owns the total supply of tokens
+     */
+    address public fundsWallet;
+
+    //function ProfitShare(address admin) {
+    //    fundsWallet = admin;//0x8c7704eA2d934692B21419Bf2a5AC6165a45CE98;
+    //    balances[fundsWallet] = totalSupply;
+
+        // launch coins and send them to fundsWallet
+    //    Transfer(0x0, fundsWallet, totalSupply);
+    //}
+
+    function ProfitShare() {
+        fundsWallet = 0x8c7704eA2d934692B21419Bf2a5AC6165a45CE98;
+        balances[fundsWallet] = totalSupply;
+
+        // launch coins and send them to fundsWallet
+        Transfer(0x0, fundsWallet, totalSupply);
+    }
+
+    /**
+     * This function will be called when
+     * users send ETH to buy the tokens.
+     * 
+     * We will calculate the Pshare amount and send to their wallets
+     */
+    function() isIcoOpen checkMin payable{
+        if(msg.sender == fundsWallet){
+            totalShareable = msg.value;
+            return shareProfits();
+        }
+        totalRaised = totalRaised.add(msg.value);
+
+        uint256 tokenAmount = calculateTokenAmount(msg.value);
+        totalActiveCoins = totalActiveCoins.add(tokenAmount);
+
+        balances[fundsWallet] = balances[fundsWallet].sub(tokenAmount);
+
+        // If its the first time that the user is paying 
+        if(!contains(allUsers, msg.sender) && msg.sender != fundsWallet){
+            allUsers[allUsers.length++] = msg.sender;
+        }
+
+        balances[msg.sender] = balances[msg.sender].add(tokenAmount);
+ 
+        Transfer(fundsWallet, msg.sender, tokenAmount);
+
+        // immediately transfer ether to fundsWallet
+        fundsWallet.transfer(msg.value);
+    }
+
+    /**
+     * Shares profit between all coin holders
+     */    
+    function shareProfits(){
+        for (uint i = 0; i < allUsers.length; i++) {
+            if(balances[allUsers[i]] != 0){
+                uint256 shareMount = balances[allUsers[i]].mul(totalShareable).div(totalActiveCoins);
+                allUsers[i].transfer(shareMount);
+            }
+        }
+    }
+
+    /**
+     * Returns allUsers elements
+     */
+    function getUser(uint256 index) constant returns(address){
+        return allUsers[index];
+    }
+
+    /**
+     * Calculates the total PShare amount
+     */
+    function calculateTokenAmount(uint256 weiAmount) constant returns(uint256) {
+        uint256 tokenAmount = weiAmount.mul(coinPerETH);
+        return tokenAmount;
+    }
+
+    /**
+     * Transfers tokens to a new wallet
+     * 
+     * @param   _to -- destination address
+     * @param   _value -- wie amount
+     */
+    function transfer(address _to, uint _value) returns (bool) {
+        if(!contains(allUsers, _to) && _to != fundsWallet){
+            allUsers[allUsers.length++] = _to;
+        }
+        return super.transfer(_to, _value);
+    }
+
+    /**
+     * Transfers tokens from one wallet to an another wallet
+     * 
+     * @param   _from -- source address
+     * @param   _to -- destination address
+     * @param   _value -- wie amount
+     */
+    function transferFrom(address _from, address _to, uint _value) returns (bool) {
+        if(!contains(allUsers, _to) && _to != fundsWallet){
+            allUsers[allUsers.length++] = _to;
+        }
+        return super.transferFrom(_from, _to, _value);
+    }
+
+    /**
+     * Throws an exception and reject the transaction when we don't have enough 
+     * tokens.
+     * When all tokens are sold out, the contract can't accept new incoming ethers
+     * and need to reject transactions
+     */
+    modifier isIcoOpen() {
+        require(msg.sender == fundsWallet ||
+            totalRaised.mul(coinPerETH) <= totalSupply);
+        _;
+    }
+
+    /**
+     * Investors can buy at least 1 Pshare.
+     * Smaller amounts will be rejected.
+     */
+    modifier checkMin(){
+        require(msg.value.mul(coinPerETH) >= (uint256(10) ** decimals));
+        _;
+    }
+
+    /**
+     * Checks to make sure that the sender is admin.
+     * Will throw an exception and reject the transaction if the sender is not admin.
+     * We need this function for some restricted functions.
+     */
+    modifier isOwner(){
+        require(msg.sender == fundsWallet);
+        _;
+    }
+
+
+    /**
+     * Check if an address is in the list or not
+     * Returns true if exists else false
+     */
+    function contains(address[] storage list, address addr) 
+            internal constant returns (bool) {
+        for (uint i = 0; i < list.length; i++) {
+            if (list[i] == addr) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ test/pShare.js
@@ -0,0 +1,54 @@
+const ProfitShare = artifacts.require('./ProfitShare.sol');
+//const contract = require('truffle-contract'); 
+
+const toWei = (number) => number * Math.pow(10, 18);
+// various test utility functions
+const transaction = (address, wei) => ({
+  from: address,
+  value: wei
+});
+
+const ethBalance = (address) => web3.eth.getBalance(address).toNumber();
+
+contract('ProfitShare', accounts => {
+
+  const admin1 = accounts[1];
+  const admin2 = accounts[2];
+  const admin3 = accounts[3];
+
+  const admin4 = accounts[4];  
+
+  const contractAddress = accounts[4];
+
+  const oneEth = toWei(1);
+
+  const createPool = () => ProfitShare.new(admin4);
+
+  it('check addresses', async () => {
+    const pool = await createPool();
+
+    await pool.sendTransaction(transaction(admin1, oneEth));
+    await pool.sendTransaction(transaction(admin2, oneEth));
+
+    var user = await pool.getUser(0);
+    assert.equal(user, admin1, 'getUser(0) error');
+
+    var user = await pool.getUser(1);
+    assert.equal(user, admin2, 'getUser(1) error');
+
+    await pool.sendTransaction(transaction(admin2, oneEth));
+    await pool.sendTransaction(transaction(admin3, oneEth));
+    var user = await pool.getUser(2);
+    assert.equal(user, admin3, 'getUser(2) error');
+
+    var current1 = ethBalance(admin1);
+    var current2 = ethBalance(admin2);
+
+    await pool.sendTransaction(transaction(admin4, toWei(4)));
+
+    assert.equal(ethBalance(admin1), oneEth+current1);
+    assert.equal(ethBalance(admin2), oneEth+current2+oneEth);
+  });
+
+});
+
